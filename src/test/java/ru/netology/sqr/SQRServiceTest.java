d3f3e7b1ef5e1fb9d81c67b7a984865bba3668fc
ksenya31 <kseniakalinnikova268@gmail.com>
2022-06-18 09:12:02 +0300

    refactor code

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,12 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SQRServiceTest {
     @ParameterizedTest
     @CsvSource(value = {"100, 200, 5",
-                        "400, 1000, 12",
-                        "200, 300, 3"})
+            "400, 1000, 12",
+            "200, 300, 3"})
     public void shouldCalc(int min, int max, int expected) {
         SQRService service = new SQRService();
         int actual = service.squaresCalculate(min, max);
-         assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
 }
