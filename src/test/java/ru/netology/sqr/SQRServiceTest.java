80da1b64cc808cdf1feff210d945c78df38c1549
ksenya31 <kseniakalinnikova268@gmail.com>
2022-06-18 09:08:15 +0300

    added test

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,16 +2,20 @@ package ru.netology.sqr;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SQRServiceTest {
-    @Test
-    public void shouldCalc() {
+    @ParameterizedTest
+    @CsvSource(value = {"100, 200, 5",
+                        "400, 1000, 12",
+                        "200, 300, 3"})
+    public void shouldCalc(int min, int max, int expected) {
         SQRService service = new SQRService();
-        int actual = service.squaresCalculate(200, 300);
-        int expected = 3;
-        assertEquals(expected, actual);
+        int actual = service.squaresCalculate(min, max);
+         assertEquals(expected, actual);
     }
 
 }
